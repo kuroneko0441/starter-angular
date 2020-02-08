df37cc61b89df92f834041947aceaa45cf0368ac
JoonYoung Lee <kuroneko0441@gmail.com>
2020-02-09 04:44:30 +0900

    refact: SharedModule에 프리셋 추가

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+const reexportedModules: any[] = [
+  CommonModule,
+  FormsModule,
+  ReactiveFormsModule,
+];
+
+const declarations: any[] = [];
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  exports: [],
+  imports: [
+    ...reexportedModules,
+  ],
+  declarations: [
+    ...declarations,
+  ],
+  exports: [
+    ...reexportedModules,
+    ...declarations,
+  ],
 })
 export class SharedModule {}
